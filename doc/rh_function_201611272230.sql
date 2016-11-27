ad1f20f8039215cf4e6e1a1e5fe5a68838f5dac1
xmmxjy <xmmxjycqupt@163.com>
2016-11-27 22:33:31 +0800

    增加才能表的插入语句

---
+++ doc/rh_function_201611272230.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO `rent-house`.rh_function (id,functionlevel,functionname,functionorder,functionurl,parent_function_id,icon,function_type,create_by,create_name,update_by,update_date,create_date,update_name,permission) VALUES 
+('A01',1,'系统管理','1','',NULL,'',NULL,'','','',NULL,NULL,'','system')
+,('A01A01',2,'用户管理','1','user/list.do','A01','',NULL,'','','',NULL,NULL,'','system.user.list')
+,('A01A02',2,'菜单管理','2','function/list.do','A01','',NULL,'','','',NULL,NULL,'','system.functioin.list')
+;
